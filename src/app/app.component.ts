e45f05dafe2837ed1616a61310e5a446d064baec
Ivan Cobos <faled.12.cobos@gmail.com>
2024-04-10 17:26:09 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+declare const $Lightning: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-iglesias-test';
+  constructor(){
+    setTimeout(() => {
+      this.renderApp();
+    }, 2000)
+  }
+  
+  renderApp(){
+    console.log("entrando a renderAPP")
+    const appName = 'c:OptOutFormWrapper';
+        const componentName = 'c:optoutForm';
+        const lightningEndpoint = 'https://trailhead1-ac-dev-ed.develop.my.site.com';
+        const targetElement = document.getElementById("prueba");
+        const componentAttributes = {
+
+        };
+        console.log("entrando a renderAPP targetElement", targetElement)
+        $Lightning.use(
+                appName,
+                function (){
+                $Lightning.createComponent(
+                        componentName,
+                        componentAttributes,
+                        targetElement,
+                        function (cmp){
+                            console.log('Componente creado');
+                        }
+                );
+                },
+                lightningEndpoint
+        );
+  }
 }
